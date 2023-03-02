621bb5ea25ba2c72ba2f2b9d4a6dde9c3bb003af
fer-cp <126120924+fer-cp@users.noreply.github.com>
2023-03-02 08:35:11 +0100

    Add files via upload

---
+++ 03_parametric_estimation/03_help_distributions_for_mle_numerically.R
@@ -0,0 +1,7 @@
+## This would be the example from class:
+?Gamma
+# https://en.wikipedia.org/wiki/Gamma_distribution
+
+## This is another very interrresting ^^ example:
+?Weibull
+# https://en.wikipedia.org/wiki/Weibull_distribution
