944cca8861bdd601f670c68cf28f28f116d936cc
fer-cp <126120924+fer-cp@users.noreply.github.com>
2023-03-08 02:00:23 +0100

    Update 03_help_distributions_for_mle_numerically.R

--- 03_parametric_estimation/03_help_distributions_for_mle_numerically.R
+++ 03_parametric_estimation/03_help_distributions_for_mle_numerically.R
@@ -1,7 +1,11 @@
 ## This would be the example from class:
-?Gamma
+?GammaDist
 # https://en.wikipedia.org/wiki/Gamma_distribution
 
 ## This is another very interrresting ^^ example:
-?Weibull
-# https://en.wikipedia.org/wiki/Weibull_distribution
+?Beta
+# https://en.wikipedia.org/wiki/Beta_distribution
+
+## Another one:
+?Chisquare
+# https://en.wikipedia.org/wiki/Chi-squared_distribution
