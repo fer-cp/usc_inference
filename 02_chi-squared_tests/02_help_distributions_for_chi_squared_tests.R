09769f3fa76a407578886c67f50c57adf61d57bc
fer-cp <126120924+fer-cp@users.noreply.github.com>
2023-03-08 01:58:11 +0100

    Add files via upload

---
+++ 02_chi-squared_tests/02_help_distributions_for_chi_squared_tests.R
@@ -0,0 +1,8 @@
+## An interrresting ^^ example:
+?Uniform
+# https://en.wikipedia.org/wiki/Continuous_uniform_distribution
+
+## This is another very interrresting ^^ example:
+?Geometric
+# https://en.wikipedia.org/wiki/Geometric_distribution
+
